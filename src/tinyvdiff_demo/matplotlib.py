2af1db4b5b9bd915a66b78b8fe3ee2abdfabd074
Nan Xiao <me@nanx.me>
2024-12-03 20:38:11 -0500

    Maybe 42 is the answer

--- src/tinyvdiff_demo/matplotlib.py
+++ src/tinyvdiff_demo/matplotlib.py
@@ -1,26 +1,22 @@
 # Each PDF generated will have different /CreationDate field within metadata
 # but visually identical
 
-import importlib.resources as pkg_resources
-
 import matplotlib.pyplot as plt
-import matplotlib.font_manager as fm
 import numpy as np
 
 
 def generate_plot(output_file):
-    font_path = pkg_resources.files("tinyvdiff_demo.fonts") / "Inter-Regular.ttf"
-    custom_font = fm.FontProperties(fname=str(font_path))
-
     x = np.linspace(0, 2 * np.pi, 100)
     y = np.sin(x)
 
+    plt.rcParams["pdf.fonttype"] = 42
+    plt.rcParams["ps.fonttype"] = 42
     plt.figure(figsize=(6, 4))
     plt.plot(x, y, label="Sine Wave")
-    plt.title("Example Plot", fontproperties=custom_font)
-    plt.xlabel("Angle [rad]", fontproperties=custom_font)
-    plt.ylabel("sin(x)", fontproperties=custom_font)
-    plt.legend(prop=custom_font)
+    plt.title("Example Plot")
+    plt.xlabel("Angle [rad]")
+    plt.ylabel("sin(x)")
+    plt.legend()
     plt.tight_layout()
     plt.savefig(output_file, format="pdf")
     plt.close()
